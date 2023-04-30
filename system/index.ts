3bb07d03589202fdaa5cc89fd414f0e40f12570e
Maxime Aubanel <maximeauba@gmail.com>
2023-04-30 23:30:10 +0200

    Update contracts addresses

--- system/index.ts
+++ system/index.ts
@@ -1,4 +1,4 @@
 export const ChainIdToStoreContract: { [chainId: number]: string } = {
-    1337: '0x17354f2d6b32007678A62D1B031f006225d54CA8', // Local
+    1337: '0x7fc2C86d96F5046F8056b8C8DDf5aA3849892C21', // Local
     5: '0x209a65b15A32278E5D7B1D5116276898fA8d6517' // Goerli
 };
